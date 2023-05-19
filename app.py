2bb7029e9ec12cc1b047d4408f40cc3ce4f6af7d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-19 13:14:16 +0200

    render index html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-    return "HELLO FROM FLASK!!!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
