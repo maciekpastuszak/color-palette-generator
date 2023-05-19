985f05d64ddb037a9e8aab745fc70460ca0f42a2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-19 13:16:22 +0200

    add route to color palette

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
+@app.route("/palette", methids=["POST"])
+def prompt_to_palette():
+    #OPEN AI COMPLETION CALL
+
+    #RETURN LIST OF COLORS
 
 @app.route("/")
 def index():
