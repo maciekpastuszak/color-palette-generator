3d2b0710825c28fc95f054d4f8ce1e286331f092
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-20 10:02:45 +0200

    integrate openai with the server

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/palette", methids=["POST"])
-def prompt_to_palette():
-    #OPEN AI COMPLETION CALL
+# @app.route("/palette", methids=["POST"])
+# def prompt_to_palette():
+# OPEN AI COMPLETION CALL
 
-    #RETURN LIST OF COLORS
+# RETURN LIST OF COLORS
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    response = openai.Completion.create(
+        model="text-davinci-003", prompt="Give me a funny word: "
+    )
+    return response["choices"][0]["text"]
+    # return render_template("index.html")
 
 
 if __name__ == "__main__":
