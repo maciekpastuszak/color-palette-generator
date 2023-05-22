1a1c8764400a48abe01fde9e73397f4908ff7590
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-22 11:49:37 +0200

    recactor scripts

--- static/app.js
+++ static/app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector("#form");
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
+    getColors();
+});
+
+function getColors(){
     const query = form.elements.query.value
     fetch("/palette", {
         method: "POST",
@@ -9,27 +14,31 @@ form.addEventListener("submit", function (e) {
         },
         body: new URLSearchParams({
             query: query
-        })
+        }),
     })
         .then((response) => response.json())
         .then(data => {
             const colors = data.colors;
             const container = document.querySelector(".container");
-            container.innerHTML = "";
-            for (const color of colors) {
-                const div = document.createElement("div");
-                div.classList.add("color");
-                div.style.backgroundColor = color;
-                div.style.width = `calc(100%/ ${colors.length})`
+            createColorBoxes(colors, container);
+        });
+}
 
-                div.addEventListener("click", function () {
-                    navigator.clipboard.writeText(color);
-                })
+function createColorBoxes(colors, container){
+    container.innerHTML = "";
+    for (const color of colors) {
+        const div = document.createElement("div");
+        div.classList.add("color");
+        div.style.backgroundColor = color;
+        div.style.width = `calc(100%/ ${colors.length})`
 
-                const span = document.createElement("span");
-                span.innerText = color;
-                div.appendChild(span);
-                container.appendChild(div);
-            }
+        div.addEventListener("click", function () {
+            navigator.clipboard.writeText(color);
         })
-})
+
+        const span = document.createElement("span");
+        span.innerText = color;
+        div.appendChild(span);
+        container.appendChild(div);
+    }
+}
